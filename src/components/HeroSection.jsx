c6453e719953416b3cd2ffd34b77e8897e6aee4c
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 07:49:44 +0200

    Stalker-commit: src/components/HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -41,9 +41,9 @@ function HeroSection() {
         </h1>
       </div>
       <div className="w-full min-h-[400px] bg-blue-500 ">
-        <div className="w-full grid grid-cols-6 overflow-clip gap-2 max-w-[900px] mx-auto">
+        <div className="w-full grid grid-cols-6 overflow-clip gap-4 max-w-[1000px] mx-auto">
           {/* 1 */}
-          <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-4">
             {images.slice(0, 2).map((image, index) => (
               <div key={index} className="w-full h-fit">
                 <img src={image.img} className="h-[200px] bg-yellow-400" />
@@ -52,7 +52,7 @@ function HeroSection() {
             ))}
           </div>
           {/* 2 */}
-          <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-4">
             {images.slice(0, 2).map((image, index) => (
               <div key={index} className="w-full h-fit">
                 <img src={image.img} className="h-[200px] bg-yellow-400" />
