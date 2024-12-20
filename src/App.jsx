acc22c64576cf5ca08a01c106d7edaaf80cd1ef0
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 14:11:48 +0200

    Stalker-commit: src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,35 @@
-import { useState } from 'react'
-import './App.css'
-import { HashRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Courses from './pages/Courses'
-import ScrollToTop from './components/ScrollTop'
-import About from './pages/About'
-import Centers from './pages/Centers'
-import CenterDetails from './pages/CenterDetails'
-import Contacts from './pages/Contacts'
+import { useState } from "react";
+import "./App.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Courses from "./pages/Courses";
+import ScrollToTop from "./components/ScrollTop";
+import About from "./pages/About";
+import Centers from "./pages/Centers";
+import CenterDetails from "./pages/CenterDetails";
+import Contacts from "./pages/Contacts";
+import HireGraduate from "./pages/HireGraduate";
 
 function App() {
-
   return (
     <>
-    <HashRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/courses' element={<Courses />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/centers' element={<Centers />} />
-        <Route path='/center-details/:center_name' element={<CenterDetails />} />
-        <Route path='/contacts' element={<Contacts />} />
-      </Routes>
-    </HashRouter>
+      <HashRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/centers" element={<Centers />} />
+          <Route
+            path="/center-details/:center_name"
+            element={<CenterDetails />}
+          />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/hireGraduate" element={<HireGraduate />} />
+        </Routes>
+      </HashRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
