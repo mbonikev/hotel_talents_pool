cbb2cf91dedc65326de477f75ffe4c69903c1baf
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 08:12:45 +0200

    Stalker-commit: src/components/HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -43,11 +43,11 @@ function HeroSection() {
   return (
     <div className="w-full h-fit max-lg:h-fit flex items-center justify-center flex-col gap-10 max-lg:gap-5 py-10 px-0 text-dark-text">
       {/* texts */}
-      <div className="mt-[90px] mb-[20px] w-full max-lg:w-full h-fit flex flex-col items-center justify-start gap-6 px-16 max-lg:px-5">
+      <div className="mt-[90px] mb-[40px] w-full max-lg:w-full h-fit flex flex-col items-center justify-start gap-6 px-16 max-lg:px-5">
         <h1 className=" text-6xl leading-[75px] font-bold tracking-tight text-center w-full">
           {HeroSectionContent.BigTitle}
         </h1>
-        <h1 className=" text-lg mt-[15px] max-lg:mt-0 leading-6 font-medium tracking-tight text-center w-full max-w-[500px] opacity-65">
+        <h1 className=" text-lg max-lg:mt-0 leading-6 font-medium tracking-tight text-center w-full max-w-[500px] opacity-65">
           {HeroSectionContent.SmallDesc}
         </h1>
         <button className="min-w-[130px] h-[42px] bg-main-color text-white font-medium text-sm flex items-center justify-center gap-1 rounded-full">
