e839aa6cd0ea60f0fb44319fdffeec0cd09f6c70
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 08:44:46 +0200

    Stalker-commit: src/components/FewCourses.jsx

--- src/components/FewCourses.jsx
+++ src/components/FewCourses.jsx
@@ -5,7 +5,7 @@ import { CoursesSection } from "../content/content";
 
 function FewCourses() {
   return (
-    <div className="w-full py-20 px-16 max-lg:px-5 max-lg:pt-0 flex flex-col text-dark-text max-w-[1000px] mx-auto">
+    <div className="w-full py-20 px-16 max-lg:px-5 max-lg:pt-0 flex flex-col text-dark-text max-w-[1700px] mx-auto">
       <div className="w-full h-fit flex flex-col gap-1">
         <h1 className="text-lg font-medium ">{CoursesSection.sectionTitle}</h1>
         <h1 className="text-5xl font-bold w-fit max-w-[300px]">{CoursesSection.HeaderTitle}</h1>
