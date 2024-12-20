a6d61ade0929c2dc235a945acfd6d80cc821bb49
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 10:10:47 +0200

    Stalker-commit: src/components/FewCourses.jsx src/components/Trainers.jsx

--- src/components/Trainers.jsx
+++ src/components/Trainers.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Trainers() {
   return (
     <div className="w-full py-20 px-16 max-lg:px-5 max-lg:pt-0 flex gap-5 items-start justify-between text-dark-text max-w-[1700px] mx-auto">
-      <div className="w-full"> </div>
+      <div className="w-full min-h-[400px] bg-[#EBEBEB]"> </div>
     </div>
   );
 }
