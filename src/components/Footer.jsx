5b8978b2df270e982df9bf540f5081dc60646195
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 11:03:18 +0200

    Stalker-commit: src/components/Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <div className="w-full bg-body-color flex flex-col items-start pt-[200px] justify-between max-md:flex-col max-md:gap-3 text-dark-text">
       <div className="bg-[#111111] w-full min-h-[300px] px-16 max-lg:px-5">
-        <div className="bg-[#b2c3ec] w-full min-h-[300px] px-16 max-lg:px-5"></div>
+        <div className="bg-[#b2c3ec] w-full min-h-[300px] px-16 max-lg:px-5 translate-y-[-150px]"></div>
       </div>
     </div>
   );
