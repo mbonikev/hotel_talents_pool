d7d62d4f2c0d9544c364bc84ea200914f13a7952
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 14:35:47 +0200

    Stalker-commit: src/pages/HireGraduate.jsx

--- src/pages/HireGraduate.jsx
+++ src/pages/HireGraduate.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import { LuSearch } from "react-icons/lu";
 import * as XLSX from "xlsx";
 
 function HireGraduate() {
+  const [graduates, setGraduates] = useState([]);
+  useEffect(() => {
+    const fetchExcelData = async () => {
+      try {
+        const response = await fetch("/data/graduates.xlsx");
+        const blob = await response.blob();
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const data = new Uint8Array(e.target.result);
+          const workbook = XLSX.read(data, { type: "array" });
+          const sheetName = workbook.SheetNames[0]; // Assuming data is in the first sheet
+          const sheet = workbook.Sheets[sheetName];
+          const jsonData = XLSX.utils.sheet_to_json(sheet);
+          setGraduates(jsonData); // jsonData will be an array of objects
+        };
+        reader.readAsArrayBuffer(blob);
+      } catch (error) {
+        console.error("Error reading Excel file:", error);
+      }
+    };
+
+    fetchExcelData();
+  }, []);
   return (
     <div className="w-full">
       <div className="w-full h-fit max-w-[1700px] mx-auto pb-[80px]">
@@ -16,11 +39,14 @@ function HireGraduate() {
           </div>
           <div className="flex items-center justify-start gap-2 border-b-[1px] border-[#b8b8b8]">
             <LuSearch className="text-xl text-[#626262]" />
-            <input type="text" className="h-[30px] outline-none border-none bg-transparent text-base placeholder:text-[#626262] text-[#111111]" placeholder="search graduate..." />
+            <input
+              type="text"
+              className="h-[30px] outline-none border-none bg-transparent text-base placeholder:text-[#626262] text-[#111111]"
+              placeholder="search graduate..."
+            />
           </div>
         </div>
         {/* graduate */}
-
       </div>
     </div>
   );
