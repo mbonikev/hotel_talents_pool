807bd27357b25f6402b1a2f9bddf1a6212ea8b49
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 08:40:16 +0200

    Stalker-commit: src/components/FewCourses.jsx

--- src/components/FewCourses.jsx
+++ src/components/FewCourses.jsx
@@ -7,7 +7,7 @@ function FewCourses() {
   return (
     <div className="w-full py-10 px-16 max-lg:px-5 max-lg:pt-0 flex flex-col text-dark-text">
       <div className="w-full h-fit grid grid-cols-2 max-lg:grid-cols-1 gap-10 max-lg:gap-2">
-        <h1 className="text-3xl font-medium ">Our Courses</h1>
+        <h1 className="text-lg font-medium ">Our Courses</h1>
         
       </div>
       {/* courses grid */}
