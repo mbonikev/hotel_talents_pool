4e83d88a40c89d3ab5f28415cfab0f95e1e9d402
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 08:51:16 +0200

    Stalker-commit: src/components/FewCourses.jsx

--- src/components/FewCourses.jsx
+++ src/components/FewCourses.jsx
@@ -5,12 +5,12 @@ import { CoursesSection } from "../content/content";
 function FewCourses() {
   return (
     <div className="w-full py-20 px-16 max-lg:px-5 max-lg:pt-0 flex gap-5 items-start justify-between text-dark-text max-w-[1700px] mx-auto">
-      <div className="w-fit min-w-fit h-fit flex flex-col gap-1 bg-red-400">
+      <div className="w-fit min-w-fit h-fit flex flex-col gap-1 ">
         <h1 className="text-lg font-medium ">{CoursesSection.sectionTitle}</h1>
         <h1 className="text-5xl font-bold w-fit max-w-[300px]">
           {CoursesSection.HeaderTitle}
         </h1>
-        <p className="text-base font-medium leading-6 max-w-[400px] pt-5 opacity-65">
+        <p className="text-base font-medium leading-6 max-w-[490px] pt-5 opacity-65">
           {CoursesSection.HeaderDesc}
         </p>
         <Link
@@ -21,7 +21,9 @@ function FewCourses() {
         </Link>
       </div>
       {/* courses grid */}
-      <div className="w-full max-w-[700px] gap-5 bg-emerald-300 h-[300px]"></div>
+      <div className="w-full max-w-[700px] gap-5 bg-emerald-300 min-h-[300px]">
+        
+      </div>
     </div>
   );
 }
