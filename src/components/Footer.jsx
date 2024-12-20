e78f0cfb17ac40c1b6aca994d5c0ecb47c2a2489
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 11:02:48 +0200

    Stalker-commit: src/components/Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className="w-full bg-body-color flex flex-col items-start justify-between max-md:flex-col max-md:gap-3 text-dark-text">
+    <div className="w-full bg-body-color flex flex-col items-start pt-[200px] justify-between max-md:flex-col max-md:gap-3 text-dark-text">
       <div className="bg-[#111111] w-full min-h-[300px] px-16 max-lg:px-5">
         <div className="bg-[#b2c3ec] w-full min-h-[300px] px-16 max-lg:px-5"></div>
       </div>
