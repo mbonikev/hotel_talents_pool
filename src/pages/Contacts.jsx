93a0da12215bbf2804a4afe2a61c81bcc947afde
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 14:05:48 +0200

    Stalker-commit: src/pages/Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -52,30 +52,8 @@ function Contacts() {
         </div>
         {/* Content */}
         <div className="h-fit w-full pb-10">
-          <div className="w-full grid grid-cols-1 max-md:grid-cols-1 px-16 py-2 max-lg:px-5">
-            <div className="w-full h-fit py-2.5 px-2 flex gap-3 items-center justify-start text-sm ">
-              <LuMail className="text-xl text-main-color ml-2" />
-              <div className="flex flex-col">
-                <span className="font-medium">Email</span>
-                <Link
-                  to="/"
-                  className="font-normal text-base hover:text-main-color hover:underline"
-                >
-                  hoteltalentspool@gmail.com
-                </Link>
-              </div>
-            </div>
-            <div className="w-full h-fit py-2.5 px-2 flex gap-3 items-center justify-start text-sm ">
-              <LuPhone className="text-xl text-main-color ml-2" />
-              <div className="flex flex-col">
-                <span className="font-medium">Phone</span>
-                <span className="font-normal text-base">+250783634802</span>
-              </div>
-            </div>
-          </div>
           {/* map */}
           <div className="w-full px-16 py-5 max-lg:px-5">
-            Address:
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51444.54513432262!2d30.059960375004472!3d-1.9577614217355783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19dca6e42ffdf0af%3A0xca7b37500ae06cc5!2sGorillas%20Golf%20Hotel!5e0!3m2!1sen!2srw!4v1730564666931!5m2!1sen!2srw"
               className="w-full h-[450px] border-none"
