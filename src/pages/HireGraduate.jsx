c3ec57ed578a04a9aef3f7533419e541e4c3daed
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 14:53:48 +0200

    Stalker-commit: src/pages/HireGraduate.jsx

--- src/pages/HireGraduate.jsx
+++ src/pages/HireGraduate.jsx
@@ -61,7 +61,7 @@ function HireGraduate() {
                     />
                   </div>
                   <div className="flex-1 flex flex-col">
-                    <h1></h1>
+                    <h1 className="text-2xl font-medium">{graduate["Full Names"]}</h1>
                   </div>
                   <button className="h-[45px] px-5 capitalize bg-main-color text-white font-medium rounded-full">view profile</button>
                 </div>
