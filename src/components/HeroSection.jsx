c4ca5655d5140b428fe2155efbe6412000541b9b
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 08:31:16 +0200

    Stalker-commit: src/components/HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -116,7 +116,7 @@ function HeroSection() {
           </div>
         </div>
         {/* white shadow */}
-        <div className="absolute -bottom-2 left-0 w-full h-[120px] border-t bg-red-300 flex items-center justify-center"></div>
+        <div className="absolute top-[83%] left-0 w-full h-[300px] border-t bg-red-300 flex items-center justify-center"></div>
       </div>
     </div>
   );
