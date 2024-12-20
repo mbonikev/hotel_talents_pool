77ad074cb0073beafa58566124d69d0d26ea441f
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 11:44:20 +0200

    Stalker-commit: src/components/HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -51,7 +51,11 @@ function HeroSection() {
         <h1 className=" text-lg max-lg:mt-0 leading-6 font-medium tracking-tight text-center w-full max-w-[500px] opacity-65">
           {HeroSectionContent.SmallDesc}
         </h1>
-        <Link to={formUrl} target="_blank" className="min-w-[130px] h-[42px] bg-main-color transition active:scale-90 text-white font-medium text-sm flex items-center justify-center gap-1 rounded-full">
+        <Link
+          to={formUrl}
+          target="_blank"
+          className="min-w-[130px] h-[42px] bg-main-color transition active:scale-90 text-white font-medium text-sm flex items-center justify-center gap-1 rounded-full"
+        >
           Enroll now
         </Link>
         <h1 className=" text-sm font-medium tracking-tight text-center w-full max-w-[500px] opacity-45">
@@ -60,14 +64,17 @@ function HeroSection() {
       </div>
       <div className="w-full min-h-[400px] overflow-x-clip flex items-start justify-center relative">
         <div className="w-fit h-fit flex items-start justify-center overflow-x-clip gap-4">
-        <div key={index} className="h-[320px] w-[240px] overflow-hidden rounded-2xl">
-                <img src={updatedImages[12]} className={`${imageStyle}`} />
-                {/* <span>{image.position}</span> */}
-              </div>
+          <div className="h-[320px] w-[240px] overflow-hidden rounded-2xl">
+            <img src={updatedImages[12]} className={`${imageStyle}`} />
+            {/* <span>{image.position}</span> */}
+          </div>
           {/* 1 */}
           <div className="flex flex-col gap-4 translate-y-[-80px]">
             {updatedImages.slice(0, 2).map((image, index) => (
-              <div key={index} className="h-[320px] w-[240px] overflow-hidden rounded-2xl">
+              <div
+                key={index}
+                className="h-[320px] w-[240px] overflow-hidden rounded-2xl"
+              >
                 <img src={image.img} className={`${imageStyle}`} />
                 {/* <span>{image.position}</span> */}
               </div>
@@ -76,7 +83,10 @@ function HeroSection() {
           {/* 2 */}
           <div className="flex flex-col gap-4 translate-y-[-130px]">
             {updatedImages.slice(2, 4).map((image, index) => (
-              <div key={index} className="h-[320px] w-[240px] overflow-hidden rounded-2xl">
+              <div
+                key={index}
+                className="h-[320px] w-[240px] overflow-hidden rounded-2xl"
+              >
                 <img src={image.img} className={`${imageStyle}`} />
                 {/* <span>{image.position}</span> */}
               </div>
@@ -85,7 +95,10 @@ function HeroSection() {
           {/* 3 */}
           <div className="flex flex-col gap-4">
             {updatedImages.slice(4, 6).map((image, index) => (
-              <div key={index} className="h-[320px] w-[240px] overflow-hidden rounded-2xl">
+              <div
+                key={index}
+                className="h-[320px] w-[240px] overflow-hidden rounded-2xl"
+              >
                 <img src={image.img} className={`${imageStyle}`} />
                 {/* <span>{image.position}</span> */}
               </div>
@@ -94,7 +107,10 @@ function HeroSection() {
           {/* 4 */}
           <div className="flex flex-col gap-4">
             {updatedImages.slice(6, 8).map((image, index) => (
-              <div key={index} className="h-[320px] w-[240px] overflow-hidden rounded-2xl">
+              <div
+                key={index}
+                className="h-[320px] w-[240px] overflow-hidden rounded-2xl"
+              >
                 <img src={image.img} className={`${imageStyle}`} />
                 {/* <span>{image.position}</span> */}
               </div>
@@ -103,7 +119,10 @@ function HeroSection() {
           {/* 5 */}
           <div className="flex flex-col gap-4 translate-y-[-130px]">
             {updatedImages.slice(8, 10).map((image, index) => (
-              <div key={index} className="h-[320px] w-[240px] overflow-hidden rounded-2xl">
+              <div
+                key={index}
+                className="h-[320px] w-[240px] overflow-hidden rounded-2xl"
+              >
                 <img src={image.img} className={`${imageStyle}`} />
                 {/* <span>{image.position}</span> */}
               </div>
@@ -112,7 +131,10 @@ function HeroSection() {
           {/* 6 */}
           <div className="flex flex-col gap-4 translate-y-[-80px]">
             {updatedImages.slice(10, 12).map((image, index) => (
-              <div key={index} className="h-[320px] w-[240px] overflow-hidden rounded-2xl">
+              <div
+                key={index}
+                className="h-[320px] w-[240px] overflow-hidden rounded-2xl"
+              >
                 <img src={image.img} className={`${imageStyle}`} />
                 {/* <span>{image.position}</span> */}
               </div>
@@ -121,10 +143,22 @@ function HeroSection() {
         </div>
         {/* white shadow */}
         <div className="absolute top-[83%] left-0 w-full h-[170px] border-t bg-body-color flex items-center justify-center gap-20">
-          <img src="https://upload.wikimedia.org/wikipedia/en/e/e4/National_Bank_of_Rwanda_logo.png" className="h-[60px] saturate-0 opacity-70" />
-          <img src="https://seeklogo.com/images/E/Equity_Bank-logo-88F49E17E2-seeklogo.com.png" className="h-[60px] saturate-0 opacity-70 py-[5px]" />
-          <img src="https://www.mineduc.gov.rw/index.php?eID=dumpFile&t=f&f=435&token=795108a09af0455f218944c43b044c38ca9da5ac" className="h-[60px] saturate-0 opacity-70 py-[5px]" />
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS4kbc2fdM1LZgPP7T1i7SAp-MpJg2AFcU7A&s" className="h-[60px] saturate-0 opacity-70 py-[5px]" />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/e/e4/National_Bank_of_Rwanda_logo.png"
+            className="h-[60px] saturate-0 opacity-70"
+          />
+          <img
+            src="https://seeklogo.com/images/E/Equity_Bank-logo-88F49E17E2-seeklogo.com.png"
+            className="h-[60px] saturate-0 opacity-70 py-[5px]"
+          />
+          <img
+            src="https://www.mineduc.gov.rw/index.php?eID=dumpFile&t=f&f=435&token=795108a09af0455f218944c43b044c38ca9da5ac"
+            className="h-[60px] saturate-0 opacity-70 py-[5px]"
+          />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS4kbc2fdM1LZgPP7T1i7SAp-MpJg2AFcU7A&s"
+            className="h-[60px] saturate-0 opacity-70 py-[5px]"
+          />
         </div>
       </div>
     </div>
