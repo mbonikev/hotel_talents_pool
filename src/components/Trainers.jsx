f3bb74f8554e276e4eeaa46c16608f8d54fde5c5
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 10:25:47 +0200

    Stalker-commit: src/components/Trainers.jsx

--- src/components/Trainers.jsx
+++ src/components/Trainers.jsx
@@ -16,7 +16,7 @@ function Trainers() {
           </p>
           <div className="w-full flex items-center justify-center gap-2">
             <div className="flex flex-col items-center justify-center w-fit gap-2">
-                <img src="https://cdn.lucidpic.com/cdn-cgi/image/w=600,format=auto,metadata=none/66c43abe18502.png" className="h-[50px] w-[50px] min-w-fit aspect-square rounded-full" />
+                <img src="https://cdn.lucidpic.com/cdn-cgi/image/w=600,format=auto,metadata=none/66c43abe18502.png" className=" h-fit min-h-[50px] w-[50px] min-w-fit aspect-square rounded-full object-cover" />
                 <h1 className="text-sm font-medium tracking-tight max-w-[50px] text-center leading-4">James Karekare</h1>
             </div>
           </div>
