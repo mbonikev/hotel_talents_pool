2536112816f19e01f84999404b5e80855e369404
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 11:48:20 +0200

    Stalker-commit: src/components/HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -161,7 +161,7 @@ function HeroSection() {
             ))}
           </div>
         </div>
-        {/* white shadow */}
+        {/* white shadow
         <div className="absolute top-[83%] left-0 w-full h-[170px] border-t bg-body-color flex items-center justify-center gap-20">
           <img
             src="https://upload.wikimedia.org/wikipedia/en/e/e4/National_Bank_of_Rwanda_logo.png"
@@ -179,7 +179,7 @@ function HeroSection() {
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS4kbc2fdM1LZgPP7T1i7SAp-MpJg2AFcU7A&s"
             className="h-[60px] saturate-0 opacity-70 py-[5px]"
           />
-        </div>
+        </div> */}
       </div>
     </div>
   );
