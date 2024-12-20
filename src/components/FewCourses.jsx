59e75c6a90de644b8e9daf3b4434ba79d1e71a4d
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 08:42:16 +0200

    Stalker-commit: src/components/FewCourses.jsx

--- src/components/FewCourses.jsx
+++ src/components/FewCourses.jsx
@@ -6,9 +6,9 @@ import { CoursesSection } from "../content/content";
 function FewCourses() {
   return (
     <div className="w-full py-10 px-16 max-lg:px-5 max-lg:pt-0 flex flex-col text-dark-text">
-      <div className="w-full h-fit flex flex-col">
+      <div className="w-full h-fit flex flex-col gap-1">
         <h1 className="text-lg font-medium ">{CoursesSection.sectionTitle}</h1>
-        <h1 className="text3xl font-bold ">{CoursesSection.HeaderTitle}</h1>
+        <h1 className="text-4xl font-bold ">{CoursesSection.HeaderTitle}</h1>
         
       </div>
       {/* courses grid */}
