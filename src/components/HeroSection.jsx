10a369ebc37895c4f91e57b045a77b8fd6acde31
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 11:45:50 +0200

    Stalker-commit: src/components/HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -64,10 +64,6 @@ function HeroSection() {
       </div>
       <div className="w-full min-h-[400px] overflow-x-clip flex items-start justify-center relative">
         <div className="w-fit h-fit flex items-start justify-center overflow-x-clip gap-4">
-          <div className="h-[320px] w-[240px] overflow-hidden rounded-2xl translate-y-[75px]">
-            <img src={updatedImages[12]} className={`${imageStyle}`} />
-            {/* <span>{image.position}</span> */}
-          </div>
           {/* 1 */}
           <div className="flex flex-col gap-4 translate-y-[-80px]">
             {updatedImages.slice(0, 2).map((image, index) => (
@@ -140,9 +136,29 @@ function HeroSection() {
               </div>
             ))}
           </div>
-          <div className="h-[320px] w-[240px] overflow-hidden rounded-2xl translate-y-[75px]">
-            <img src={updatedImages[12]} className={`${imageStyle}`} />
-            {/* <span>{image.position}</span> */}
+          {/* 7 */}
+          <div className="flex flex-col gap-4 translate-y-[-80px]">
+            {updatedImages.slice(10, 12).map((image, index) => (
+              <div
+                key={index}
+                className="h-[320px] w-[240px] overflow-hidden rounded-2xl"
+              >
+                <img src={image.img} className={`${imageStyle}`} />
+                {/* <span>{image.position}</span> */}
+              </div>
+            ))}
+          </div>
+          {/* 8 */}
+          <div className="flex flex-col gap-4 translate-y-[-80px]">
+            {updatedImages.slice(10, 12).map((image, index) => (
+              <div
+                key={index}
+                className="h-[320px] w-[240px] overflow-hidden rounded-2xl"
+              >
+                <img src={image.img} className={`${imageStyle}`} />
+                {/* <span>{image.position}</span> */}
+              </div>
+            ))}
           </div>
         </div>
         {/* white shadow */}
