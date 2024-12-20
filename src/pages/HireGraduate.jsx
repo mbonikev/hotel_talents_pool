d0e9c9e917fe4abc305561da92ddb5ed848d3428
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 14:12:48 +0200

    Stalker-commit: src/pages/HireGraduate.jsx

--- src/pages/HireGraduate.jsx
+++ src/pages/HireGraduate.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
 
 function HireGraduate() {
   return (
-    <div>HireGraduate</div>
-  )
+    <div className="w-full">
+      <div className="w-full h-fit max-w-[1700px] mx-auto pb-[80px]">
+        <Navbar />
+        <div className="w-full flex px-16 py-24 justify-between">
+          <div className="w-fit h-fit flex flex-col items-start justify-center gap-2">
+            <h1 className="text-5xl font-bold w-fit text-dark-text/90">
+              Hire Graduate
+            </h1>
+            <p className="text-base font-normal text-[#626262] max-w-[434px]">
+              We offer hands-on training for aspiring professionals and PWDs,
+              helping them gain the skills and confidence to build successful,
+              fulfilling careers in Rwanda’s growing hospitality industry.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default HireGraduate
+export default HireGraduate;
