730497233100d0e8e92d30aaf77f70a608b228ab
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 10:12:17 +0200

    Stalker-commit: src/components/Trainers.jsx

--- src/components/Trainers.jsx
+++ src/components/Trainers.jsx
@@ -5,7 +5,7 @@ function Trainers() {
     <div className="w-full py-20 px-16 max-lg:px-5 max-lg:pt-0 flex gap-5 items-start justify-between text-dark-text max-w-[1700px] mx-auto">
       <div className="w-full min-h-[400px] bg-[#EBEBEB] rounded-3xl relative">
         {/* lights */}
-        <div className="w-[200px] aspect-square absolute top-0 left-0"></div>
+        <div className="w-[200px] aspect-square absolute top-0 left-0 bg-[#7FA2CA]/60"></div>
       </div>
     </div>
   );
