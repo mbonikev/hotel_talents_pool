34fd2e2b56e017cc28ed47f72189ed1e7ab26864
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 08:26:15 +0200

    Stalker-commit: src/components/Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ function Navbar({setLoading}) {
     <div className="w-full flex items-center justify-between px-5 py-2.5 text-dark-text bg-body-color sticky top-0 z-30">
       {/* logo */}
       <div className="w-[120px] max-w-[120px] flex justify-center overflow-hidden">
-        <img src="./logo.png" className="h-16 w-[120px] block object-contain" />
+        <img src="./logo.png" className="h-9 w-[120px] block object-contain" />
       </div>
       {/* links */}
       <div
