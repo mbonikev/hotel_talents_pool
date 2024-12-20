d8de7ce87ae4a54708671599370725ef3fb0fbc8
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 10:15:47 +0200

    Stalker-commit: src/components/Trainers.jsx

--- src/components/Trainers.jsx
+++ src/components/Trainers.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Trainers() {
   return (
     <div className="w-full py-20 px-16 max-lg:px-5 max-lg:pt-0 flex gap-5 items-start justify-between text-dark-text max-w-[1700px] mx-auto">
-      <div className="w-full min-h-[400px] bg-[#EBEBEB] rounded-3xl relative overflow-clip">
+      <div className="w-full h-[400px] bg-[#EBEBEB] rounded-3xl relative overflow-clip">
         {/* lights */}
         <div className="w-[40%] rounded-full blur-2xl aspect-square absolute top-[-90px] right-[-90px] bg-[#7FA2CA]/60"></div>
-        <div className="w-[40%] rounded-full blur-2xl aspect-square absolute bottom-[-50%] left-[-90px] bg-[#4D61BA]/35"></div>
+        <div className="w-[40%] rounded-full blur-2xl aspect-square absolute bottom-[-350px] left-[-90px] bg-[#4D61BA]/35"></div>
       </div>
     </div>
   );
