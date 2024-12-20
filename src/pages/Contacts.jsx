cdc12267d0e7be9154c5c471164d2f88f84bf3f5
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 13:59:48 +0200

    Stalker-commit: src/pages/Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -9,15 +9,17 @@ function Contacts() {
     <div className="w-full">
       <div className="w-full h-fit max-w-[1700px] mx-auto pb-[80px]">
         <Navbar />
-        <div className="w-full h-fit p-20 flex flex-col items-start justify-center gap-2">
-          <h1 className="text-5xl font-bold w-fit text-dark-text/90">
-            Contacts
-          </h1>
-          <p className="text-base font-normal text-[#626262] max-w-[434px]">
-            We offer hands-on training for aspiring professionals and PWDs,
-            helping them gain the skills and confidence to build successful,
-            fulfilling careers in Rwanda’s growing hospitality industry.
-          </p>
+        <div className="w-full flex justify-between">
+          <div className="w-fit h-fit p-20 flex flex-col items-start justify-center gap-2">
+            <h1 className="text-5xl font-bold w-fit text-dark-text/90">
+              Contacts
+            </h1>
+            <p className="text-base font-normal text-[#626262] max-w-[434px]">
+              We offer hands-on training for aspiring professionals and PWDs,
+              helping them gain the skills and confidence to build successful,
+              fulfilling careers in Rwanda’s growing hospitality industry.
+            </p>
+          </div>
         </div>
         {/* Content */}
         <div className="h-fit w-full pb-10">
