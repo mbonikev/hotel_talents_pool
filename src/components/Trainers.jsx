64183a1f3b6987a35c767fae974cd251dda9e14b
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 10:29:47 +0200

    Stalker-commit: src/components/Trainers.jsx

--- src/components/Trainers.jsx
+++ src/components/Trainers.jsx
@@ -14,18 +14,23 @@ function Trainers() {
           <p className="text-lg font-medium max-w-[490px] pt-3 opacity-65">
             We offer hands-on training for aspiring
           </p>
-          <div className="w-full flex items-center justify-center gap-2 mt-4">
-            <div className="flex flex-col items-center justify-center w-fit gap-2">
-              <div className="h-[70px] w-[70px] min-w-fit aspect-square rounded-full flex items-center justify-center overflow-hidden ">
-                <img
-                  src="https://cdn.lucidpic.com/cdn-cgi/image/w=600,format=auto,metadata=none/66c43abe18502.png"
-                  className=" h-fit min-h-full w-full object-cover"
-                />
-              </div>
-              <h1 className="text-base font-medium tracking-tight max-w-[100px] text-center leading-5">
-                James Karekare
-              </h1>
-            </div>
+          <div className="w-full flex items-center justify-center gap-2 mt-7">
+            {[...Array(5)].map((_, index) => (
+              <>
+                {/* person */}
+                <div className="flex flex-col items-center justify-center w-fit gap-2">
+                  <div className="h-[70px] w-[70px] min-w-fit aspect-square rounded-full flex items-center justify-center overflow-hidden ">
+                    <img
+                      src="https://cdn.lucidpic.com/cdn-cgi/image/w=600,format=auto,metadata=none/66c43abe18502.png"
+                      className=" h-fit min-h-full w-full object-cover"
+                    />
+                  </div>
+                  <h1 className="text-base font-medium tracking-tight max-w-[100px] text-center leading-5">
+                    James Karekare
+                  </h1>
+                </div>
+              </>
+            ))}
           </div>
         </div>
       </div>
