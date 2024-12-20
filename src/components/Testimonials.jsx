45e44ae49eec5cb16b1aa37f8f077766116fb623
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-20 10:56:18 +0200

    Stalker-commit: src/components/Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -22,10 +22,10 @@ function Testimonials() {
       <div className="w-full columns-3">
         {/* testimonial */}
         {TestimonialsSection.testimonials.map((person, index) => (
-          <div className="flex flex-col gap-4 items-center justify-center text-center break-inside-avoid bg-white relative p-5 rounded-2xl overflow-hidden">
+          <div className="flex flex-col gap-4 items-center justify-center text-center break-inside-avoid bg-white relative p-5 rounded-2xl overflow-hidden ring-1 ring-stone-200">
             {/* lights */}
-            <div className="w-[120px] rounded-full -z-0 blur-[0px] aspect-square absolute top-[-20px] right-[-20px] bg-red-500/60"></div>
-            <div className="w-[120px] rounded-full -z-0 blur-[0px] aspect-square absolute bottom-[-20px] left-[-20px] bg-blue-500/35"></div>
+            <div className="w-[120px] rounded-full -z-0 blur-[80px] aspect-square absolute top-[-20px] right-[-20px] bg-red-500/60"></div>
+            <div className="w-[120px] rounded-full -z-0 blur-[80px] aspect-square absolute bottom-[-20px] left-[-20px] bg-blue-500/35"></div>
             <div className="w-full h-full z-10 flex flex-col items-center justify-center gap-1">
               <div className="flex items-center justify-start gap-7">
                 <div className="h-16 w-auto aspect-square relative">
