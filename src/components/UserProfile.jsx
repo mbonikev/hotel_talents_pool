c51c5eb35dc9e9101f92cd0ddf622f7bf03bdad3
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-21 08:16:48 +0200

    updated src/components/UserProfile.jsx

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -57,8 +57,10 @@ function UserProfile({ Hide, data }) {
               </div>
               <div className="flex-1 flex flex-col w-full h-full gap-3 py-3 overscroll-contain overflow-y-auto custom_scrollbar">
                 <div className="flex items-start justify-start w-full gap-2">
-                  <TbHexagonFilled className="text-purple-600 text-sm mt-[3px] min-w-fit" />
-                  <h1 className="text-sm text-dark-text/70">Lorem</h1>
+                  <TbHexagonFilled className="text-main-color text-sm mt-[3px] min-w-fit" />
+                  <h1 className="text-sm text-dark-text/70">
+                  <strong className="text-dark-text"></strong>
+                  </h1>
                 </div>
               </div>
             </div>
