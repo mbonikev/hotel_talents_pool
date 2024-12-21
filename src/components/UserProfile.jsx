5868b7952f9078f001629368248cbfff5b63811f
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-21 08:17:18 +0200

    updated src/components/UserProfile.jsx

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -59,7 +59,7 @@ function UserProfile({ Hide, data }) {
                 <div className="flex items-start justify-start w-full gap-2">
                   <TbHexagonFilled className="text-main-color text-sm mt-[3px] min-w-fit" />
                   <h1 className="text-sm text-dark-text/70">
-                  <strong className="text-dark-text"></strong>
+                  <strong className="text-dark-text">Full Names</strong>
                   </h1>
                 </div>
               </div>
