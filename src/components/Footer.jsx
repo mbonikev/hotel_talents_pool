84968c6c363ce5b54054c747c1db18e42a61e91b
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-21 09:59:00 +0200

    updated src/components/Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,7 +33,7 @@ function Footer() {
         </div>
       </div>
       {/* bottom part */}
-      <div className="bg-[#111111] w-full pt-[150px] ">
+      <div className="bg-[#111111] w-full pt-[150px] max-md:pt-[170px] ">
         <div className="w-full h-fit flex items-center justify-center flex-col px-16 max-lg:px-5 max-w-[1700px] mx-auto">
           <div className=" w-full h-fit flex-1 px-12 py-12 max-md:px-0 max-md:py-0 mt-4 flex justify-between items-start max-lg:flex-col max-md:gap-10">
             <div className="flex h-full flex-col items-start justify-between">
