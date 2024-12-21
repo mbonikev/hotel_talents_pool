f83bc65bc3907b824375a9d4420c8d5486331f67
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-21 09:57:30 +0200

    updated src/components/HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -49,7 +49,7 @@ function HeroSection() {
   return (
     <div className="w-full h-fit max-lg:h-fit flex items-center justify-center flex-col gap-10 max-lg:gap-5 py-10 px-0  text-dark-text">
       {/* texts */}
-      <div className="mt-[90px] max-lg:mt-10 mb-[40px] max-lg:mb-20 w-full max-lg:w-full h-fit flex flex-col items-center justify-start gap-6 max-md:gap-4 max-md:items-start px-16 max-lg:px-5">
+      <div className="mt-[90px] max-lg:mt-10 mb-[40px] max-lg:mb-24 w-full max-lg:w-full h-fit flex flex-col items-center justify-start gap-6 max-md:gap-4 max-md:items-start px-16 max-lg:px-5">
         <h1 className=" text-6xl leading-[75px] font-bold max-sm:text-4xl max-md:text-left max-md:pr-10 tracking-tight text-center w-full">
           {HeroSectionContent.BigTitle}
         </h1>
@@ -96,7 +96,7 @@ function HeroSection() {
             ))}
           </div>
           {/* 3 */}
-          <div className="flex flex-col gap-4 translate-y-[-80px] max-md:translate-y-[-10px]">
+          <div className="flex flex-col gap-4 translate-y-[-80px] max-md:translate-y-[-15px]">
             {updatedImages.slice(4, 6).map((image, index) => (
               <div
                 key={index}
