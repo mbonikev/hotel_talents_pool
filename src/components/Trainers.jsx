a14970767cb30fdb1ccbfc75456efc7b2d1c9bba
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-21 09:30:59 +0200

    updated src/components/Trainers.jsx

--- src/components/Trainers.jsx
+++ src/components/Trainers.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Trainers() {
   return (
-    <div className="w-full h-fit min-h-[400px] bg-red-300 px-16 max-lg:px-5 max-lg:pt-0 flex gap-5 flex-col items-start justify-between text-dark-text max-w-[1700px] mx-auto">
+    <div className="w-full h-fit min-h-[400px] max-md:min-h-fit bg-red-300 px-16 max-lg:px-5 max-lg:pt-0 flex gap-5 flex-col items-start justify-between text-dark-text max-w-[1700px] mx-auto">
       <div className="w-full flex-1 h-full p-10 flex flex-col bg-[#EBEBEB] rounded-3xl relative overflow-clip">
         {/* lights */}
         <div className="w-[40%] rounded-full -z-0 blur-[100px] aspect-square absolute top-[-150px] right-[-160px] bg-[#7FA2CA]/60"></div>
@@ -16,20 +16,17 @@ function Trainers() {
           </p>
           <div className="bg-orange-500 w-full h-fit flex items-center justify-center flex-wrap gap-2 mt-7">
             {[...Array(7)].map((_, index) => (
-              <>
-                {/* person */}
-                <div className="flex flex-col items-center justify-center w-fit gap-2">
-                  <div className="h-[80px] w-[80px] min-w-fit aspect-square rounded-full flex items-center justify-center overflow-hidden ">
-                    <img
-                      src="https://cdn.lucidpic.com/cdn-cgi/image/w=600,format=auto,metadata=none/66c43abe18502.png"
-                      className=" h-fit min-h-full w-full object-cover"
-                    />
-                  </div>
-                  <h1 className="text-base font-medium tracking-tight max-w-[100px] text-center leading-5">
-                    James Karekare
-                  </h1>
+              <div className="flex flex-col items-center justify-center w-fit gap-2">
+                <div className="h-[80px] w-[80px] min-w-fit aspect-square rounded-full flex items-center justify-center overflow-hidden ">
+                  <img
+                    src="https://cdn.lucidpic.com/cdn-cgi/image/w=600,format=auto,metadata=none/66c43abe18502.png"
+                    className=" h-fit min-h-full w-full object-cover"
+                  />
                 </div>
-              </>
+                <h1 className="text-base font-medium tracking-tight max-w-[100px] text-center leading-5">
+                  James Karekare
+                </h1>
+              </div>
             ))}
           </div>
         </div>
