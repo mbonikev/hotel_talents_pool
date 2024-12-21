3766844c900126aef35a87d7dc8c5e9bbb23bfac
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-21 09:24:59 +0200

    updated src/components/Trainers.jsx

--- src/components/Trainers.jsx
+++ src/components/Trainers.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Trainers() {
   return (
     <div className="w-full py-20 px-16 max-lg:px-5 max-lg:pt-0 flex gap-5 items-start justify-between text-dark-text max-w-[1700px] mx-auto">
-      <div className="w-full h-[400px] bg-[#EBEBEB] rounded-3xl relative overflow-clip">
+      <div className="w-full h-[400px] max-lg:h-full bg-[#EBEBEB] rounded-3xl relative overflow-clip">
         {/* lights */}
         <div className="w-[40%] rounded-full -z-0 blur-[100px] aspect-square absolute top-[-150px] right-[-160px] bg-[#7FA2CA]/60"></div>
         <div className="w-[40%] rounded-full -z-0 blur-[100px] aspect-square absolute bottom-[-150px] left-[-160px] bg-[#4D61BA]/35"></div>
