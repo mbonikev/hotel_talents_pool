b155d2b972e7e7c5cd61c4e9a21c52861aa968dc
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-21 09:43:59 +0200

    updated src/components/Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
       {/* floating */}
       <div className="w-full flex flex-col items-start justify-between max-md:flex-col mt-[160px] max-md:gap-3 text-dark-text relative max-w-[1700px] mx-auto">
         <div className="w-full h-[300px] px-16 max-lg:px-5 absolute top-[-150px] ">
-          <div className="w-full h-full bg-[#b2c3ec] rounded-[35px] p-10 flex items-start justify-between max-md:flex-col">
+          <div className="w-full h-full bg-[#b2c3ec] rounded-[35px] p-10 max-sm:p-6 flex items-start justify-between max-md:flex-col">
             <div className="w-full flex-1">
               <h1 className="text-4xl leading-[45px] max-lg:text-2xl tracking-tight pb-3 font-extrabold w-fit text-[#111111]">
                 Apply today and <br /> kick start your hospitality <br /> Career
