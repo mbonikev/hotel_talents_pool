f656296b77318a1e66e753d2f2e55685f8d6eaa7
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-22 16:48:46 +0200

    updated src/pages/About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -97,16 +97,6 @@ function About() {
                   <h1 className="text-xl font-bold">President</h1>
                   <h1 className="text-lg">President</h1>
                 </div>
-                <div className="flex items-center justify-center w-fit max-sm:w-full break-inside-avoid flex-col">
-                  <div className="h-[250px] max-sm:h-fit w-auto max-lg:w-full aspect-square relative overflow-hidden bg-white rounded-2xl mb-4 flex items-center justify-center">
-                    <img
-                      src="/images/Leaders/2.jpg"
-                      className="h-fit min-h-full w-full object-cover brightness-90"
-                    />
-                  </div>
-                  <h1 className="text-xl font-bold">President</h1>
-                  <h1 className="text-lg">President</h1>
-                </div>
               </div>
             </div>
           </div>
