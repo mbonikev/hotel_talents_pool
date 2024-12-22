615ff018aee205835a57a3e3dc96dc4a5f93b3ed
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-22 15:58:29 +0200

    updated src/pages/About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -86,8 +86,8 @@ function About() {
             <div className="w-1/2 h-fit flex flex-col gap-1 px-16 max-md:px-5 ">
               <h1 className="text-lg font-bold text-main-color">Leaders</h1>
               <h1 className="text-5xl max-lg:text-4xl font-bold w-fit">Meet The Leaders</h1>
-              <div className="flex items-start justify-start gap-5 mt-8">
-                <div className="flex items-center justify-center flex-col">
+              <div className="flex items-start justify-start flex-wrap gap-5 mt-8">
+                <div className="flex items-center justify-center max-lg:w-full flex-col">
                   <div className="h-[250px] w-auto aspect-square relative overflow-hidden bg-white rounded-2xl mb-4 flex items-center justify-center">
                     <img
                       src="/images/Leaders/1.jpg"
@@ -97,7 +97,7 @@ function About() {
                   <h1 className="text-xl font-bold">President</h1>
                   <h1 className="text-lg">President</h1>
                 </div>
-                <div className="flex items-center justify-center flex-col">
+                <div className="flex items-center justify-center max-lg:w-full flex-col">
                   <div className="h-[250px] w-auto aspect-square relative overflow-hidden bg-white rounded-2xl mb-4 flex items-center justify-center">
                     <img
                       src="/images/Leaders/2.jpg"
