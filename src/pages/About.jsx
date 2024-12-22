613fb6062ebb2ee6089cb90b833faf4fe66b9874
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-22 16:01:29 +0200

    updated src/pages/About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -83,10 +83,10 @@ function About() {
             </div>
 
             {/* leaders */}
-            <div className="w-1/2 h-fit flex flex-col gap-1 px-16 max-md:px-5 ">
+            <div className="w-full h-fit flex flex-col gap-1 px-16 max-md:px-5 ">
               <h1 className="text-lg font-bold text-main-color">Leaders</h1>
               <h1 className="text-5xl max-lg:text-4xl font-bold w-fit">Meet The Leaders</h1>
-              <div className="max-lg:w-full columns-2 bg-red-200 gap-5 mt-8">
+              <div className="w-full columns-3 bg-red-200 p-2 gap-5 mt-8">
                 <div className="flex items-center justify-center w-fit break-inside-avoid flex-col">
                   <div className="h-[250px] w-auto max-lg:w-full aspect-square relative overflow-hidden bg-white rounded-2xl mb-4 flex items-center justify-center">
                     <img
