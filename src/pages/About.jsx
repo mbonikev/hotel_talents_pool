0245ac272acf81939195d36849c45b1a57c82797
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-22 15:56:29 +0200

    updated src/pages/About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -63,7 +63,7 @@ function About() {
                 The fundamental principles guiding our team
               </h1>
               {/* value grid */}
-              <div className="w-fit grid grid-cols-2 max-lg:grid-cols-1 gap-4 mt-[50px]">
+              <div className="w-fit grid grid-cols-2 max-lg:flex max-lg:flex-col gap-4 mt-[50px]">
                 {[
                   { color: "bg-main-color/30", value: "Everyone Belongs" },
                   { color: "bg-yellow-500/30", value: "We Get Things Done" },
