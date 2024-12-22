96bc0189eb577f5f888cbd5d1e34d474fa88516d
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-22 16:38:15 +0200

    updated src/pages/TermsAndConditions.jsx

---
+++ src/pages/TermsAndConditions.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+
+const TermsAndConditions = () => {
+  const [content, setContent] = useState('');
+
+  useEffect(() => {
+    fetch('/src/content/privacy-policy.md')
+      .then((response) => response.text())
+      .then((text) => setContent(text));
+  }, []);
+
+  return (
+    <div className='markdown-container'>
+      <ReactMarkdown>{content}</ReactMarkdown>
+    </div>
+  );
+};
+
+export default TermsAndConditions;
