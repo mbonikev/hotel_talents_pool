6dd8324c1136b1c4f2a1aee1a0400a9ef8498705
kevindermot99 <65622766+kevindermot99@users.noreply.github.com>
2024-12-22 16:04:59 +0200

    updated src/pages/Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -12,7 +12,7 @@ function Contacts() {
     <div className="w-full">
       <div className="w-full h-fit max-w-[1700px] mx-auto pb-[80px]">
         <Navbar />
-        <div className="w-full flex px-16 py-24 justify-between">
+        <div className="w-full flex px-16 py-24 justify-between max-lg:flex-col">
           <div className="w-fit h-fit flex flex-col items-start justify-center gap-2">
             <h1 className="text-5xl font-bold w-fit text-dark-text/90">
               Contact us
