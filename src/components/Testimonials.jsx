7b8ee61a5588f9e2fd73b05390d0f46acced05fd
mbonikev <mbonimpayekevin@gmail.com>
2025-01-27 11:08:46 +0200

    updated src/components/Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -31,7 +31,7 @@ function Testimonials() {
                 <div className="h-12 w-12 min-w-12 aspect-square rounded-full flex items-center justify-center relative overflow-hidden">
                   <img
                     src={person.img}
-                    className="w-full  object-cover "
+                    className="w-full h-fit object-cover "
                   />
                 </div>
                 <div className="flex flex-col items-start justify-start">
