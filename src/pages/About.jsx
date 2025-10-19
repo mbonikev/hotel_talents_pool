747abab7a02a5a074cc7ecc6f4b2fa8c9a104ccc
mbonikev <mbonimpayekevin@gmail.com>
2025-10-19 20:23:05 +0200

    sponsor logo + CEO picture

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -27,7 +27,7 @@ function About() {
                 <h1 className="text-lg font-bold text-main-color">
                   Get to know us
                 </h1>
-                <h1 className="text-5xl font-bold w-fit max-w-[300px]">
+                <h1 className="text-5xl font-bold w-fit max-w-[400px]">
                   Who we are?
                 </h1>
                 <p className="text-base font-medium leading-6 max-w-[590px] text-justify pt-5 opacity-65">
@@ -81,138 +81,39 @@ function About() {
             </div>
 
             {/* leaders */}
-            {/* <div className="w-full h-fit flex flex-col gap-1 px-16 max-md:px-0 ">
+            <div className="w-full h-fit flex flex-col gap-1 px-16 max-md:px-0 ">
               <h1 className="text-lg font-bold text-main-color">Leaders</h1>
-              <h1 className="text-5xl max-lg:text-4xl font-bold w-fit">Meet The Leaders</h1>
-              <div className="w-full flex items-start justify-center flex-wrap p-2 gap-9 max-lg:gap-8 mt-8">
-                <div className="flex items-center justify-center w-fit max-sm:w-full break-inside-avoid flex-col">
+              <h1 className="text-5xl max-lg:text-4xl font-bold w-fit">
+                Meet The Leaders
+              </h1>
+              <div className="w-full flex items-start justify-start flex-wrap p-2 gap-9 max-lg:gap-8 mt-8">
+                {/* <div className="flex items-center justify-center w-fit max-sm:w-full break-inside-avoid flex-col">
                   <div className="h-[180px] max-sm:h-fit w-auto max-lg:w-full aspect-square relative overflow-hidden bg-white rounded-full mb-4 flex items-center justify-center">
                     <img
                       src="/images/Leaders/1.jpg"
                       className="h-fit min-h-full w-full object-cover brightness-90"
                     />
                   </div>
-                  <h1 className="text-xl font-bold">Name</h1>
+                  <h1 className="text-xl font-bold my-1">Name</h1>
                   <h1 className="text-lg">President</h1>
-                </div>
-                
+                </div> */}
+
                 <div className="flex items-center justify-center w-fit max-sm:w-full break-inside-avoid flex-col">
                   <div className="h-[180px] max-sm:h-fit w-auto max-lg:w-full aspect-square relative overflow-hidden bg-white rounded-full mb-4 flex items-center justify-center">
                     <img
-                      src="/images/Leaders/2.jpg"
+                      src="/images/Leaders/3.png"
                       className="h-fit min-h-full w-full object-cover brightness-90"
                     />
                   </div>
-                  <h1 className="text-xl font-bold">Name</h1>
-                  <h1 className="text-lg">President</h1>
-                </div>
-                
-                <div className="flex items-center justify-center w-fit max-sm:w-full break-inside-avoid flex-col">
-                  <div className="h-[180px] max-sm:h-fit w-auto max-lg:w-full aspect-square relative overflow-hidden bg-white rounded-full mb-4 flex items-center justify-center">
-                    <img
-                      src="/images/Leaders/2.jpg"
-                      className="h-fit min-h-full w-full object-cover brightness-90"
-                    />
-                  </div>
-                  <h1 className="text-xl font-bold">Name</h1>
-                  <h1 className="text-lg">President</h1>
-                </div>
-                
-                <div className="flex items-center justify-center w-fit max-sm:w-full break-inside-avoid flex-col">
-                  <div className="h-[180px] max-sm:h-fit w-auto max-lg:w-full aspect-square relative overflow-hidden bg-white rounded-full mb-4 flex items-center justify-center">
-                    <img
-                      src="/images/Leaders/2.jpg"
-                      className="h-fit min-h-full w-full object-cover brightness-90"
-                    />
-                  </div>
-                  <h1 className="text-xl font-bold">Name</h1>
-                  <h1 className="text-lg">President</h1>
-                </div>
-                
-                <div className="flex items-center justify-center w-fit max-sm:w-full break-inside-avoid flex-col">
-                  <div className="h-[180px] max-sm:h-fit w-auto max-lg:w-full aspect-square relative overflow-hidden bg-white rounded-full mb-4 flex items-center justify-center">
-                    <img
-                      src="/images/Leaders/2.jpg"
-                      className="h-fit min-h-full w-full object-cover brightness-90"
-                    />
-                  </div>
-                  <h1 className="text-xl font-bold">Name</h1>
-                  <h1 className="text-lg">President</h1>
-                </div>
-                
-                <div className="flex items-center justify-center w-fit max-sm:w-full break-inside-avoid flex-col">
-                  <div className="h-[180px] max-sm:h-fit w-auto max-lg:w-full aspect-square relative overflow-hidden bg-white rounded-full mb-4 flex items-center justify-center">
-                    <img
-                      src="/images/Leaders/2.jpg"
-                      className="h-fit min-h-full w-full object-cover brightness-90"
-                    />
-                  </div>
-                  <h1 className="text-xl font-bold">Name</h1>
-                  <h1 className="text-lg">President</h1>
-                </div>
-                
-                <div className="flex items-center justify-center w-fit max-sm:w-full break-inside-avoid flex-col">
-                  <div className="h-[180px] max-sm:h-fit w-auto max-lg:w-full aspect-square relative overflow-hidden bg-white rounded-full mb-4 flex items-center justify-center">
-                    <img
-                      src="/images/Leaders/2.jpg"
-                      className="h-fit min-h-full w-full object-cover brightness-90"
-                    />
-                  </div>
-                  <h1 className="text-xl font-bold">Name</h1>
-                  <h1 className="text-lg">President</h1>
-                </div>
-                
-                <div className="flex items-center justify-center w-fit max-sm:w-full break-inside-avoid flex-col">
-                  <div className="h-[180px] max-sm:h-fit w-auto max-lg:w-full aspect-square relative overflow-hidden bg-white rounded-full mb-4 flex items-center justify-center">
-                    <img
-                      src="/images/Leaders/2.jpg"
-                      className="h-fit min-h-full w-full object-cover brightness-90"
-                    />
-                  </div>
-                  <h1 className="text-xl font-bold">Name</h1>
-                  <h1 className="text-lg">President</h1>
-                </div>
-                
-                <div className="flex items-center justify-center w-fit max-sm:w-full break-inside-avoid flex-col">
-                  <div className="h-[180px] max-sm:h-fit w-auto max-lg:w-full aspect-square relative overflow-hidden bg-white rounded-full mb-4 flex items-center justify-center">
-                    <img
-                      src="/images/Leaders/2.jpg"
-                      className="h-fit min-h-full w-full object-cover brightness-90"
-                    />
-                  </div>
-                  <h1 className="text-xl font-bold">Name</h1>
-                  <h1 className="text-lg">President</h1>
-                </div>
-                
-                <div className="flex items-center justify-center w-fit max-sm:w-full break-inside-avoid flex-col">
-                  <div className="h-[180px] max-sm:h-fit w-auto max-lg:w-full aspect-square relative overflow-hidden bg-white rounded-full mb-4 flex items-center justify-center">
-                    <img
-                      src="/images/Leaders/2.jpg"
-                      className="h-fit min-h-full w-full object-cover brightness-90"
-                    />
-                  </div>
-                  <h1 className="text-xl font-bold">Name</h1>
-                  <h1 className="text-lg">President</h1>
-                </div>
-                
-                <div className="flex items-center justify-center w-fit max-sm:w-full break-inside-avoid flex-col">
-                  <div className="h-[180px] max-sm:h-fit w-auto max-lg:w-full aspect-square relative overflow-hidden bg-white rounded-full mb-4 flex items-center justify-center">
-                    <img
-                      src="/images/Leaders/2.jpg"
-                      className="h-fit min-h-full w-full object-cover brightness-90"
-                    />
-                  </div>
-                  <h1 className="text-xl font-bold">Name</h1>
-                  <h1 className="text-lg">President</h1>
+                  <h1 className="text-xl font-semibold my-1">GISANURA Christian</h1>
+                  <h1 className="text-lg text-dark-text/70">CEO</h1>
                 </div>
               </div>
-            </div> */}
+            </div>
             <div className="w-full mt-20 max-lg:mt-8 px-16 py-5 max-lg:px-0 max-lg:pt-0 flex gap-10 items-center justify-between max-lg:flex-col text-dark-text max-w-[1700px] mx-auto">
               <div className="w-full max-lg:w-full h-fit flex flex-col gap-1 ">
-                <h1 className="text-5xl font-bold w-fit">
-                  Our Sponsors
-                </h1>
-                <p className="text-base font-medium leading-6 text-justify pt-5 opacity-65">
+                <h1 className="text-5xl font-bold w-fit">Our Sponsors</h1>
+                {/* <p className="text-base font-medium leading-6 text-justify pt-5 opacity-65">
                   Hotel Talents Pool (HTP) proudly operates with the support of
                   Africa for Excellence Mission (AFEM), an NGO dedicated to
                   promoting education and career growth across Africa. AFEM's
@@ -229,7 +130,21 @@ function About() {
                   >
                     Africa for Excellence Mission
                   </a>
-                </p>
+                </p> */}
+                <div className="w-full flex items-start justify-start flex-wrap gap-4">
+                  <div className="h-[180px] flex items-center justify-center">
+                    <img
+                      src="/images/Sponsors/Sangira.png"
+                      className="h-full object-cover brightness-90"
+                    />
+                  </div>
+                  <div className="h-[180px] flex items-center justify-center">
+                    <img
+                      src="/images/Sponsors/AFEM.png"
+                      className="h-full object-cover brightness-90"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
